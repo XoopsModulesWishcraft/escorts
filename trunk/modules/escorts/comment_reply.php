97823e66d4b2663f6b54280104565256a46bbc3f
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-04-10 02:56:25 +0000

    Removing Trunk from Chronolabs Modules

--- trunk/modules/escorts/comment_reply.php
+++
@@ -1,9 +0,0 @@
-<?php
-//  ------------------------------------------------------------------------ //
-// Author: Ben Brown                                                         //
-// Site: http://xoops.thehandcoders.com                                      //
-// Project: The XOOPS Project                                                //
-// ------------------------------------------------------------------------- //
-include '../../mainfile.php';
-include XOOPS_ROOT_PATH.'/include/comment_reply.php';
-?>
--- trunk/modules/escorts/view.tag.php
+++
@@ -1,4 +0,0 @@
-<?php
-include "../../mainfile.php";
-include XOOPS_ROOT_PATH . "/modules/tag/view.tag.php";
-?>
